5cde5c699eb261e0b0376b08c3bdc7161eb1c547
Prettyface1 <prettyface1@gmail.com>
2025-12-29 22:22:17 +0100

    feat: wallet selector implementation step 5

--- frontend/src/components/WalletSelector.tsx
+++ frontend/src/components/WalletSelector.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { Button } from './ui/Button';
 import { authenticate } from '../lib/stacks';
 import { initWC } from '../lib/walletconnect';
+export const WalletSelector = () => {
