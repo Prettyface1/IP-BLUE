5864fe2c4176435dd21c6672ef60ef06f916fc8a
Prettyface1 <prettyface1@gmail.com>
2025-12-29 18:01:10 +0100

    feat: app assembly step 10

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,3 +7,4 @@ const App = () => {
     <div className='flex h-screen overflow-hidden'>
       <Sidebar className='w-64 border-r border-border' />
       <div className='flex-1 flex flex-col'>
+        <Navbar className='h-16 border-b border-border' />
