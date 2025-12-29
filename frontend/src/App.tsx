a3075a29f71b8135983d18077898855da513390a
Prettyface1 <prettyface1@gmail.com>
2025-12-29 18:01:06 +0100

    feat: app assembly step 5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Navbar } from './components/ui/Navbar';
 import { Sidebar } from './components/ui/Sidebar';
+const App = () => {
