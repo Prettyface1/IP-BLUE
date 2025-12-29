29b79c1613616288c48b55927cc72f2b6ff7f15c
Prettyface1 <prettyface1@gmail.com>
2025-12-29 17:25:32 +0100

    feat: chainhooks-client setup step 5

--- frontend/src/lib/chainhooks.ts
+++ frontend/src/lib/chainhooks.ts
@@ -2,3 +2,4 @@ import { ChainhooksClient } from '@hirosystems/chainhooks-client';
 export const setupChainhooks = () => {
   const client = new ChainhooksClient({
     baseUrl: 'https://api.hirosystems.com',
+    apiKey: 'YOUR_API_KEY'
