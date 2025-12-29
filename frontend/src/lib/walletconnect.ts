bd337b67801a826d83ddf8f28f98422bd6305729
Prettyface1 <prettyface1@gmail.com>
2025-12-29 17:24:44 +0100

    feat: walletconnect integration step 5

--- frontend/src/lib/walletconnect.ts
+++ frontend/src/lib/walletconnect.ts
@@ -2,3 +2,4 @@ import SignClient from '@walletconnect/sign-client';
 export const initWC = async () => {
   const signClient = await SignClient.init({
     projectId: 'YOUR_PROJECT_ID',
+    metadata: {
