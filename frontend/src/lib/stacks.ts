fbdca251fa273ee181531fbe71994719313d0b7c
Prettyface1 <prettyface1@gmail.com>
2025-12-29 17:24:11 +0100

    feat: stacks-connect implementation step 5

--- frontend/src/lib/stacks.ts
+++ frontend/src/lib/stacks.ts
@@ -2,3 +2,4 @@ import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 import { StacksMainnet, StacksTestnet } from '@stacks/network';
 const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const userSession = new UserSession({ appConfig });
+export const network = new StacksTestnet();
