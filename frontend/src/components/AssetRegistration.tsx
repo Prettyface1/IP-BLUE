98882da42b2301be370332b2cb2a095b253970ab
Prettyface1 <prettyface1@gmail.com>
2025-12-29 22:22:53 +0100

    feat: registration form implementation step 5

--- frontend/src/components/AssetRegistration.tsx
+++ frontend/src/components/AssetRegistration.tsx
@@ -2,3 +2,4 @@ import React, { useState } from 'react';
 import { Input } from './ui/Input';
 import { Button } from './ui/Button';
 export const AssetRegistration = () => {
+  const [title, setTitle] = useState('');
